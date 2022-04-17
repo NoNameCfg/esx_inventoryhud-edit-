6566856ec34f59de5b0e64c159c8c48e739fd77e
NoNameCfg <65350561+NoNameCfg@users.noreply.github.com>
2022-04-17 11:22:20 +0100

    Add files via upload

---
+++ esx_inventoryhud/fxmanifest.lua
@@ -0,0 +1,44 @@
+fx_version "adamant"
+game "gta5"
+
+author "F1"
+
+description "ESX Inventory HUD - EDIT"
+
+version "2.4.0"
+
+ui_page "html/ui.html"
+
+client_scripts {
+  "@es_extended/locale.lua",
+  "client/main.lua",
+  "client/trunk.lua",
+  "client/property.lua",
+  "client/player.lua",
+  "client/shops.lua",
+  "client/storage.lua",
+  "locales/*.lua",
+  "config.lua"
+}
+
+server_scripts {
+  "@es_extended/locale.lua",
+  "server/main.lua",
+  "server/storage.lua",
+  "locales/*.lua",
+  "config.lua"
+}
+
+files {
+  "html/ui.html",
+  "html/css/ui.css",
+  "html/css/jquery-ui.css",
+  "html/js/inventory.js",
+  "html/js/config.js",
+  -- JS LOCALES
+  "html/locales/*.js",
+  -- IMAGES
+  "html/img/*.png",
+  -- ICONS
+  "html/img/items/*.png"
+}
